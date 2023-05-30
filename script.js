4cc8ff2844b828ec286fa372962e8b2059ea3f6b
JorgeOyolaMoreno <jorgeoyola.pm@gmail.com>
2023-05-30 15:46:51 -0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,30 @@
+function showAlert() {
+    alert("Alerta con onclick");
+}
+
+let boton = document.getElementById("boti");
+boton.addEventListener("click",function(){alert("alerta con oyente de eventos")})
+
+
+
+const form = document.getElementById("myForm");
+myForm.addEventListener("submit",function(event){event.preventDefault();validateForm()})
+
+function validateEmail(email) {
+    const regex =  /^[^\s@]+@[^\s@]+\.[^\s@]{2,7}$/
+    return regex.test(email)
+  }
+  function validateForm() {
+    const emailInput = document.getElementById('email');
+    const email = emailInput.value;
+    if (!validateEmail(email)) {
+      alert('Por favor ingrese un correo electrónico válido.');
+    } else {
+      alert('Correo electrónico enviado correctamente.');
+    }
+  }
+
+  document.querySelector("button.button-menu-toggle")
+    .addEventListener("click", function() {
+           document.querySelector(".nav-links").
+                      classList.toggle("nav-links-responsive")})
